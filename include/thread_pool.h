82e2c4822a4883b78124add781894aedb906f937
dds <1934423564@qq.com>
2024-04-16 22:17:33 +0800

    google style

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -9,100 +9,103 @@
 #include <queue>
 #include <thread>
 
-template <typename T> class ts_queue {
+template <typename T>
+class ts_queue {
 public:
-  ts_queue() = default;
-  ts_queue(const ts_queue &) = delete;
-  ts_queue(const ts_queue &&) = delete;
-  ~ts_queue() = default;
+    ts_queue() = default;
+    ts_queue(const ts_queue &) = delete;
+    ts_queue(const ts_queue &&) = delete;
+    ~ts_queue() = default;
 
-  void push(const T &t) {
-    std::lock_guard lock(mtx_);
-    q_.emplace(t);
-  }
-
-  bool pop(T &t) {
-    std::lock_guard lock(mtx_);
-    if (q_.empty()) {
-      return false;
+    void push(const T &t) {
+        std::lock_guard lock(mtx_);
+        q_.emplace(t);
     }
-    t = std::move(q_.front());
-    q_.pop();
-    return true;
-  }
 
-  bool empty() {
-    std::lock_guard lock(mtx_);
-    return q_.empty();
-  }
+    bool pop(T &t) {
+        std::lock_guard lock(mtx_);
+        if (q_.empty()) {
+            return false;
+        }
+        t = std::move(q_.front());
+        q_.pop();
+        return true;
+    }
+
+    bool empty() {
+        std::lock_guard lock(mtx_);
+        return q_.empty();
+    }
 
 private:
-  std::queue<T> q_;
-  std::mutex mtx_;
+    std::queue<T> q_;
+    std::mutex mtx_;
 };
 
 class thread_pool {
 public:
-  thread_pool() = default;
-  thread_pool(const thread_pool &) = delete;
-  thread_pool(thread_pool &&) = delete;
-  ~thread_pool() { close(); }
+    thread_pool() = default;
+    thread_pool(const thread_pool &) = delete;
+    thread_pool(thread_pool &&) = delete;
+    ~thread_pool() { close(); }
 
-  void start(std::size_t n) {
-    open_ = true;
-    for (int i = 0; i < n; i++) {
-      threads_.push_back(std::thread{std::bind(&thread_pool::work, this)});
+    void start(std::size_t n) {
+        open_ = true;
+        for (int i = 0; i < n; i++) {
+            threads_.push_back(
+                std::thread{std::bind(&thread_pool::work, this)});
+        }
     }
-  }
 
-  void close() {
-    while (!q_.empty()) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    void close() {
+        while (!q_.empty()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+        open_ = false;
+        cv_.notify_all();
+        for (auto &thd : threads_) {
+            if (thd.joinable()) {
+                thd.join();
+            }
+        }
+        threads_.clear();
     }
-    open_ = false;
-    cv_.notify_all();
-    for (auto &thd : threads_) {
-      if (thd.joinable()) {
-        thd.join();
-      }
-    }
-    threads_.clear();
-  }
 
-  template <typename F, typename... Args> auto submit(F &&f, Args &&...args) {
-    using return_t = decltype(f(args...));
-    std::function<return_t()> task =
-        std::bind(std::forward<F>(f), std::forward<Args>(args)...);
-    auto task_ptr = std::make_shared<std::packaged_task<return_t()>>(task);
-    q_.push([task_ptr]() { (*task_ptr)(); });
-    cv_.notify_one();
-    return task_ptr->get_future();
-  }
+    template <typename F, typename... Args>
+    auto submit(F &&f, Args &&...args) {
+        using return_t = decltype(f(args...));
+        std::function<return_t()> task =
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+        auto task_ptr = std::make_shared<std::packaged_task<return_t()>>(task);
+        q_.push([task_ptr]() { (*task_ptr)(); });
+        cv_.notify_one();
+        return task_ptr->get_future();
+    }
 
 private:
-  ts_queue<std::function<void()>> q_;
-  std::vector<std::thread> threads_;
-  std::mutex mtx_;
-  std::condition_variable cv_;
-  bool open_{false};
+    ts_queue<std::function<void()>> q_;
+    std::vector<std::thread> threads_;
+    std::mutex mtx_;
+    std::condition_variable cv_;
+    bool open_{false};
 
-  void work() {
-    std::function<void()> func;
-    bool get;
-    while (open_) {
-      {
-        std::unique_lock lock(mtx_);
-        cv_.wait(lock, [this]() { return !open_ || !q_.empty(); });
-        if (!open_) {
-          return;
+    void work() {
+        std::function<void()> func;
+        bool get;
+        while (open_) {
+            {
+                std::unique_lock lock(mtx_);
+                cv_.wait(lock, [this]() { return !open_ || !q_.empty(); });
+                if (!open_) {
+                    return;
+                }
+                get = q_.pop(func);
+            }
+            if (get) {
+                func();
+            }
         }
-        get = q_.pop(func);
-      }
-      if (get) {
-        func();
-      }
     }
-  }
 };
 
 #endif
--- src/g_test.cpp
+++ src/g_test.cpp
@@ -1,45 +1,46 @@
-#include "../include/thread_pool.h"
 #include <bits/stdc++.h>
 #include <gtest/gtest.h>
+
+#include "../include/thread_pool.h"
 
 using ll = long long;
 std::vector<ll> mem;
 
 ll fib(int k) {
-  if (mem[k] != -1) {
-    return mem[k];
-  }
-  if (k == 0 || k == 1) {
-    return 1;
-  }
-  return mem[k] = fib(k - 1) + fib(k - 2);
+    if (mem[k] != -1) {
+        return mem[k];
+    }
+    if (k == 0 || k == 1) {
+        return 1;
+    }
+    return mem[k] = fib(k - 1) + fib(k - 2);
 }
 
 TEST(thread_pool_test, thread_pool) {
-  int fib_max = 10000;
-  std::vector<ll> fib_v(fib_max, 1);
-  mem.assign(fib_max, -1);
-  for (int i = 2; i < fib_max; i++) {
-    fib_v[i] = fib_v[i - 1] + fib_v[i - 2];
-  }
-  thread_pool tp{};
-  tp.start(16);
+    int fib_max = 10000;
+    std::vector<ll> fib_v(fib_max, 1);
+    mem.assign(fib_max, -1);
+    for (int i = 2; i < fib_max; i++) {
+        fib_v[i] = fib_v[i - 1] + fib_v[i - 2];
+    }
+    thread_pool tp{};
+    tp.start(16);
 
-  std::vector<ll> answer;
-  std::vector<std::future<ll>> fts;
-  int iter = 10000;
-  for (int i = 0; i < iter; i++) {
-    int k = rand() % fib_max;
-    fts.push_back(tp.submit(fib, k));
-    answer.push_back(fib_v[k]);
-  }
-  tp.close();
-  for (int i = 0; i < iter; i++) {
-    EXPECT_EQ(answer[i], fts[i].get());
-  }
+    std::vector<ll> answer;
+    std::vector<std::future<ll>> fts;
+    int iter = 10000;
+    for (int i = 0; i < iter; i++) {
+        int k = rand() % fib_max;
+        fts.push_back(tp.submit(fib, k));
+        answer.push_back(fib_v[k]);
+    }
+    tp.close();
+    for (int i = 0; i < iter; i++) {
+        EXPECT_EQ(answer[i], fts[i].get());
+    }
 }
 
 int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
